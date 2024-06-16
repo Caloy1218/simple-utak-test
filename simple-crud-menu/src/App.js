aebb03508eae09e516a5231e7af7f23d3a64aa64
Caloy1218 <jcsaquing1218@gmail.com>
2024-06-16 21:51:15 +0800

    Added some style to MenuList and MenuItemForm component

--- simple-crud-menu/src/App.js
+++ simple-crud-menu/src/App.js
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-
 import MenuItemForm from './Components/MenuItemForm/MenuItemForm';
 import MenuList from './Components/MenuList/MenuList';
 
 const App = () => {
   const [currentItem, setCurrentItem] = useState(null);
+  const [menuItems, setMenuItems] = useState([]);
 
   return (
     <div>
-      <h1>Restaurant Menu</h1>
-      <MenuItemForm currentItem={currentItem} setCurrentItem={setCurrentItem} />
-      <MenuList setCurrentItem={setCurrentItem} />
+      <MenuItemForm currentItem={currentItem} setCurrentItem={setCurrentItem} menuItems={menuItems} />
+      <MenuList setCurrentItem={setCurrentItem} setMenuItems={setMenuItems} menuItems={menuItems} />
     </div>
   );
 };
